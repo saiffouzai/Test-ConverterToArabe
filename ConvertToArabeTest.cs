979caf71eb081d42278506df59d0d3b234f6ab64
saiffouzai <46888809+saiffouzai@users.noreply.github.com>
2020-10-08 10:29:26 +0200

    Add project files
    Add files

---
+++ ConvertToArabeTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FnacTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class ConverterRomainToArabeTest
+    {
+
+        private Converter _converter;
+        private Converter converter
+        {
+            get
+            {
+                if(_converter == null)
+                {
+                    _converter = new Converter();
+                }
+                return _converter;
+            }
+            set
+            {
+                _converter = value;
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnArabe1WhenRomainIsokI()
+        {
+            int resultConvertion = converter.ConvertRomainNumberToArabe("I");
+            Assert.AreEqual(1, resultConvertion);
+        }
+
+        [TestMethod]
+        public void ShouldReturnArabe2WhenRomainIsokII()
+        {
+            int resultConvertion = converter.ConvertRomainNumberToArabe("II");
+            Assert.AreEqual(2, resultConvertion);
+        }
+
+        [TestMethod]
+        public void ShouldReturnArabe10WhenRomainIsokX()
+        {
+            int resultConvertion = converter.ConvertRomainNumberToArabe("X");
+            Assert.AreEqual(10, resultConvertion);
+        }
+
+        [TestMethod]
+        public void ShouldReturnArabe100WhenRomainIsokM()
+        {
+            int resultConvertion = converter.ConvertRomainNumberToArabe("M");
+            Assert.AreEqual(1000, resultConvertion);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldThrowExceptionWhenRomainIsNotOk()
+        {
+            int resultConvertion = converter.ConvertRomainNumberToArabe("H");
+        }
+
+        [TestCleanup]
+        public void MyTestCleanup()
+        {
+            converter = null;
+        }
+        
+    }
+}
---
+++ Converter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FnacTest
+{
+    public class Converter
+    {
+        private Dictionary<char, int> dictionary = new Dictionary<char, int>
+                                                       {
+                                                           {'I', 1},
+                                                           {'V', 5},
+                                                           {'X', 10},
+                                                           {'L', 50},
+                                                           {'C', 100},
+                                                           {'D', 500},
+                                                           {'M', 1000}
+                                                       };
+
+        public int ConvertRomainNumberToArabe(string romain)
+        {
+            char[] romainArray = romain.ToCharArray();
+            int maxBrothers = 0, previouseValue = 0, resultat = 0;
+
+            foreach (char number in romainArray)
+            {
+                if (!dictionary.ContainsKey(number))
+                {
+                    throw new ArgumentException("Impossible de convertir ce nombre");
+                }
+
+                var auxNumber = dictionary[number];
+
+                if (auxNumber == 1000)
+                {
+                    resultat += auxNumber;
+                    maxBrothers = 0;
+                    previouseValue = 0;
+                    continue;
+                }
+
+                if (auxNumber == previouseValue)
+                {
+                    maxBrothers++;
+                }
+
+                if (maxBrothers <= 2)
+                {
+                    resultat += auxNumber;
+                    previouseValue = auxNumber;
+                }
+                else
+                {
+
+                    switch (maxBrothers)
+                    {
+                        case 3:
+                            previouseValue = auxNumber;
+                            maxBrothers++;
+                            break;
+                        case 4:
+                            int newValue = auxNumber - previouseValue;
+                            resultat += newValue;
+                            maxBrothers = 0;
+                            previouseValue = 0;
+                            break;
+                    }
+                }
+            }
+            return resultat;
+        }
+    }
+}
